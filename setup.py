d01c129a4f962294f8bf4ff1f89e94ae5b79d6c7
pieterbartsmit <pieterbartsmit@gmail.com>
2024-05-27 11:01:18 -0700

    Added calculation of skewness from a spectrum

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="linearwavetheory",
-    version="0.0.10",
+    version="0.0.11",
     license="Apache 2 License",
     install_requires=[
         "numpy",
