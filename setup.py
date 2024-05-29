dd013f0192d4086823c3eafa692f5547c98c3418
pieterbartsmit <pieterbartsmit@gmail.com>
2024-05-29 12:53:07 -0700

    Commit for release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="linearwavetheory",
-    version="0.0.16",
+    version="0.0.17",
     license="Apache 2 License",
     install_requires=[
         "numpy",
