7d16374b2012eddc0716f85902fce86907132dd9
pieterbartsmit <pieterbartsmit@gmail.com>
2025-05-23 18:00:28 -0700

    - Passes all tests - Estimate the primary spectrum from a nonlinear spectrum - Add tests for the Lagrangian coeficients - Bump version number

--- src/linearwavetheory/stokes_theory/_estimate_primary_spectra.py
+++ src/linearwavetheory/stokes_theory/_estimate_primary_spectra.py
@@ -5,6 +5,7 @@ import numpy as np
 from typing import Union
 from numba import prange, jit
 from .._numba_settings import numba_default
+from linearwavetheory._utils import _direction_bin
 
 
 @jit(**numba_default)
@@ -40,19 +41,40 @@ def estimate_primary_spectrum_from_nonlinear_spectra(
     spectra = np.zeros(
         (nspec, nfreq, ndir),
     )
+    spectra[:, :, :] = variance_density[:, :, :]
+
+    delta_angle = _direction_bin(angle_degrees, wrap=360)
     for i in prange(nspec):
         if progress_bar is not None:
             progress_bar.update(1)
 
-        for iter in range(0, 10):
-            spectra[i, :, :] = _point_bound_surface_wave_spectrum_2d(
+        for iter in range(0, 3):
+            bound_estimate = _point_bound_surface_wave_spectrum_2d(
                 intrinsic_angular_frequency,
                 angle_degrees,
-                variance_density[i, :, :],
+                spectra[i, :, :],
                 depth[i],
                 output,
                 nonlinear_options,
                 physics_options,
             )
+            spectra[i, :, :] = variance_density[i, :, :] - bound_estimate
+
+            # delta = raw_estimate - spectra[i, :, :]
+            # spectra[i, :, :] = spectra[i, :, :] + under_relaxation_factor * delta
+
+            for ii in range(nfreq):
+                total = 0.0
+                target = 0.0
+                for jj in range(ndir):
+                    target += spectra[i, ii, jj] * delta_angle[jj]
+                    if spectra[i, ii, jj] < 0:
+                        spectra[i, ii, jj] = 0
+                    total += spectra[i, ii, jj] * delta_angle[jj]
+
+                if target < 0.0:
+                    target = 0.0
+
+                spectra[i, ii, :] = spectra[i, ii, :] * np.abs((target / total))
 
     return spectra.reshape(dims)
